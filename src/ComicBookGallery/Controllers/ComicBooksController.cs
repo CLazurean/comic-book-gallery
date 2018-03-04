1828cceb4cefd0b68cb8760b2b923ece2d6c3b07
Cristian Lazurean <cristian_lazurean@hotmail.com>
2018-03-04 09:11:09 +0200

    Updated detail view, data in controller

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,15 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The spirderman";
+            ViewBag.IssueNumber = "123";
+            ViewBag.Description = "qwerty <strong>QWERTY</strong";
+            ViewBag.Artists = new string[]
+                {
+                    "asdfgh",
+                    "zxcvbn"
+                };
+
             return View();
         }
     }
