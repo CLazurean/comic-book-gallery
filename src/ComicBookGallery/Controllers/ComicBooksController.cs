91a1506637f444b5c243001405f3afb12dde07ca
Cristian Lazurean <cristian_lazurean@hotmail.com>
2018-03-04 10:19:35 +0200

    updated comic book detail view layout

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -13,9 +13,9 @@ namespace ComicBookGallery.Controllers
         {
             var comicBook = new ComicBook()
             {
-                SeriesTitle = "The spirderman",
-                IssueNumber = 123,
-                DescriptionHtml = "qwerty <strong>QWERTY</strong",
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                 {
                     new Artist(){ Name="Dan Slott", Role="Script" },
